9439f5a7beaa0baf9877b6ed616f99aaec31498b
Fotis Kyritsakis <101351920+fkiritsakis@users.noreply.github.com>
2022-03-24 10:17:01 +0200

    Further expanded the script.js but still not enough to work properly
    We need the script to run as the page loads and not expect the user to input a text file manually

--- restaurant/script.js
+++ restaurant/script.js
@@ -1,3 +1,48 @@
+//NOT WORKING YET
+//See this for help https://stackoverflow.com/questions/23331546/how-to-use-javascript-to-read-local-text-file-and-read-line-by-line
+
+//Reading from a file that is saved with the id filename in the html document
+document.getElementById('file').onchange = function()
+{
+    var file = this.files[0];
+
+    var reader = new FileReader();
+    reader.onload = function(progressEvent)
+    {
+        var lines = this.result.split('\n');
+        for(var line = 0; line < lines.length; line++)
+        {
+            //Here goes the code to create a new div element
+            //Fill said code with the data from the lines that the program
+            //reads from the file(Format of file: One line is product name, next
+            //line is product price with two decimals)
+            
+            let div = document.createElement('div');
+
+            div.className = 'divClassName';
+
+            //creating two h2 elements to show name and price of product
+            let productName = document.createElement('h2');
+            let productPrice = document.createElement('h2');
+
+            productName.textContent = 'This is the text that will show the name of the product';
+            productPrice.textContent = 'This is the text that represents the price of the document';
+
+            productName.className = 'productNameClass';
+            productPrice.className = 'productPriceClass';
+
+            div.appendChild(productName);
+            div.appendChild(productPrice);
+
+            document.body.appendChild(div);
+
+        }
+
+        alert('It works this far');
+    }
+}
+
+
 //creating a new div element
 let div = document.createElement('div');
 
@@ -21,3 +66,8 @@ document.body.appendChild(div);
 
 //Anything that we want to add to the created div should always be
 //before we append the said div into the body of the document
+
+//TBD add an onload function to the body element in the html function
+//In said onload function in javascript we should read the file line by line
+//and then store each line to a temporary variable and display the data accordingly
+//by appending html elements 
