5ba6dfc38d9c1192abbb965aa34a0ec5c3f983d6
Fotis Kyritsakis <101351920+fkiritsakis@users.noreply.github.com>
2022-07-18 13:46:33 +0300

    something with the mdf

--- VisualStudioProjects/BankingSystem/BankingSystem/frmDashboard.Designer.cs
+++ VisualStudioProjects/BankingSystem/BankingSystem/frmDashboard.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmDashboard));
             this.pnlTopPanel = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.btnMinimize = new System.Windows.Forms.Button();
@@ -100,6 +101,7 @@
             this.Controls.Add(this.pnlTopPanel);
             this.Font = new System.Drawing.Font("Leelawadee UI", 8.25F);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmDashboard";
             this.Text = "frmDashboard";
             this.pnlTopPanel.ResumeLayout(false);
