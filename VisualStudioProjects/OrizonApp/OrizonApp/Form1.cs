2dfcc0375010716df039a389d675ff0e1206f5af
Fotis Kyritsakis <101351920+fkiritsakis@users.noreply.github.com>
2022-07-23 14:45:25 +0300

    Created a new Visual Studio Project called OrizonApp
    Created a new application to be used in the company. Within said project I started creating the two first forms, the Login Form and the Dashboard. Currently all the text is in Greek but should later add Localization for English.

---
+++ VisualStudioProjects/OrizonApp/OrizonApp/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OrizonApp
+{
+    public partial class FrmLogin : Form
+    {
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            //Creating a new dashboard form
+            FrmDashboard frmDashboard = new FrmDashboard();
+            //Setting the reference to the login form in the newly created dashboard form
+            frmDashboard.refToFrmLogin = this;
+            //Hiding this form
+            this.Visible = false;
+            //Showing the dashboard
+            frmDashboard.Show();
+        }
+    }
+}
---
+++ VisualStudioProjects/OrizonApp/OrizonApp/FrmDashboard.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OrizonApp
+{
+    public partial class FrmDashboard : Form
+    {
+
+        //Reference to FrmLogin
+        public FrmLogin refToFrmLogin { get; set; }
+
+        public FrmDashboard()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            //Showing the login form and closing the dashboard
+            this.refToFrmLogin.Show();
+            this.Close();
+        }
+    }
+}
